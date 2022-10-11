3325044ae28c8a0c87328adafb061c5e4c31cce8
Murtajiz Kazmi <mhk37@njit.edu>
2022-10-11 15:18:04 -0400

    new

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,16 +1,19 @@
 package com.codepath.apps.restclienttemplate.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 import org.json.JSONObject
 
-class Tweet {
 
-    var body: String = ""
-    var createdAt: String = ""
-    var user: User? = null
+@Parcelize
+class Tweet(var body: String = "", var createdAt: String = "", var user: User? = null):
 
-    companion object{
-        fun fromJson(jsonObject: JSONObject) : Tweet{
+Parcelable {
+
+
+    companion object {
+        fun fromJson(jsonObject: JSONObject): Tweet {
             val tweet = Tweet()
             tweet.body = jsonObject.getString("text")
             tweet.createdAt = jsonObject.getString("created_at")
@@ -20,12 +23,11 @@ class Tweet {
 
         fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
             val tweets = ArrayList<Tweet>()
-            for (i in 0 until jsonArray.length()){
+            for (i in 0 until jsonArray.length()) {
                 tweets.add(fromJson(jsonArray.getJSONObject(i)))
             }
             return tweets
         }
 
     }
-
 }
